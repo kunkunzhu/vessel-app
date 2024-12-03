0bff67839072c7e6248c5779d5004eed4f3a4b8c
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-12-03 16:45:28 -0500

    add placeholder add word flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,13 @@
 "use client";
 
 import { Collection } from "@/components/Collection";
+import { Button } from "@/components/form/Button";
 import { NavHeader } from "@/components/NavHeader";
 import { Search } from "@/components/Search";
 import { Viewmenu } from "@/components/Viewmenu";
 import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const sampleWords = [
   "paradigm",
@@ -20,11 +21,27 @@ const sampleWords = [
   "pleasantries",
   "pliability",
   "pretentious",
+  "paradigm",
+  "parochial",
+  "paternalistic",
+  "patronizing",
+  "physicomorphs",
+  "physiurgic",
+  "pleasantries",
+  "pliability",
+  "pretentious",
 ];
+
+interface AddModalI {
+  open: boolean;
+  closeModal: () => void;
+}
 
 export default function Home() {
   const { user } = useAuthContext();
   const router = useRouter();
+
+  const [modal, setModal] = useState<boolean>(false);
 
   useEffect(() => {
     if (!user) {
@@ -33,15 +50,24 @@ export default function Home() {
   }, [user]);
 
   return (
-    <>
+    <div className="px-20 flex flex-col gap-6">
       <NavHeader />
-      <div className="flex items-center gap-20 pl-40 pt-8">
+      <div className="flex items-center gap-20 pt-8">
         <Viewmenu />
         <Search />
       </div>
-      <div className="px-40 mt-20 ">
-        <Collection words={sampleWords} />
+      <div className="flex justify-start mt-10">
+        <div className="w-[200px]">
+          <Button
+            text="add word"
+            onClick={() => setModal(true)}
+            classname="h-fit py-2 bg-white text-primary border border-primary hover:bg-accent hover:text-background hover:border-white transition-all"
+          />
+        </div>
+        <div className="px-14">
+          <Collection words={sampleWords} />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
