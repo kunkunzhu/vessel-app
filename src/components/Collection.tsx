51298cb420defef8dc576cc48e3cccd812a92a5d
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-12-13 17:12:12 -0500

    init word modal flow

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,18 +1,35 @@
 /** @format */
 
-function Word({ word }: { word: string }) {
+import { WordI } from "@/lib/types";
+
+function Word({
+  word,
+  selectWord,
+}: {
+  word: WordI;
+  selectWord: (word: WordI) => void;
+}) {
   return (
-    <div className="bg-secondary cursor-crosshair hover:bg-accent hover:text-background transition-all rounded-lg px-4 py-1 text-lg font-title">
-      {word}
+    <div
+      className="bg-secondary cursor-crosshair hover:bg-accent hover:text-background transition-all rounded-lg px-4 py-1 text-lg font-title"
+      onClick={() => selectWord(word)}
+    >
+      {word.word}
     </div>
   );
 }
 
-export function Collection({ words }: { words: string[] }) {
+export function Collection({
+  words,
+  selectWord,
+}: {
+  words: WordI[];
+  selectWord: (word: WordI) => void;
+}) {
   return (
     <div className="flex flex-wrap gap-4">
       {words.map((word, index) => (
-        <Word word={word} key={index} />
+        <Word word={word} key={index} selectWord={selectWord} />
       ))}
     </div>
   );
