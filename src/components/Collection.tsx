f3b1f167ab4a65b496adde4894ef1c7305b44067
Kun Zhu <zkzhu@uwaterloo.ca>
2025-02-02 12:09:53 -0500

    add word display

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -2,6 +2,12 @@
 
 import { WordI } from "@/lib/types";
 
+interface CollectionI {
+  words: WordI[];
+  selectWord: (word: WordI) => void;
+  // view: "lexico" | "chrono";
+}
+
 function Word({
   word,
   selectWord,
@@ -22,15 +28,36 @@ function Word({
 export function Collection({
   words,
   selectWord,
-}: {
-  words: WordI[];
-  selectWord: (word: WordI) => void;
-}) {
+  // view
+}: CollectionI) {
+
+  const lexicallyCategorizedWords = words.reduce((categories, word) => {
+    const firstLetter = word.word[0].toUpperCase();
+    if (!categories[firstLetter]) {
+      categories[firstLetter] = [];
+    }
+    categories[firstLetter].push(word);
+    return categories;
+  }, {} as Record<string, WordI[]>);
+  const letters = Object.keys(lexicallyCategorizedWords).sort();
+
+
   return (
-    <div className="flex flex-wrap gap-4">
-      {words.map((word, index) => (
-        <Word word={word} key={index} selectWord={selectWord} />
-      ))}
+    <div className="flex flex-col gap-6 overflow-scroll">
+      {
+        letters.map((letter) => (
+          <div key={letter} className="flex flex-col gap-2">
+            <div className="w-[1000px] border-b-[0.5px] border-secondary">
+              <h2 className="text-md text-secondary font-title">{letter}</h2>
+            </div>
+            <div className="flex flex-wrap gap-4">
+              {lexicallyCategorizedWords[letter].map((word, index) => (
+                <Word word={word} key={index} selectWord={selectWord} />
+              ))}
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 }
